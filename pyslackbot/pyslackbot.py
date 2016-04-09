afac237b9b54d54533afd615def55c827a2d6080
Greg McCoy <gregmccoy@gfa.org>
2016-04-09 17:04:30 +0000

    Now uses JSON, supports multiple messages and replies

--- pyslackbot/pyslackbot.py
+++ pyslackbot/pyslackbot.py
@@ -1,7 +1,8 @@
 from slackclient import SlackClient
 import threading
 import time
-import csv
+import json
+import random
 
 class SlackBot(object):
     def __init__(self, bot_id, debug=False):
@@ -27,25 +28,34 @@ class SlackBot(object):
                 return h
         return None
 
-    def add_handler(self, handler_id, msg, reply, run=None, channel=None, con=True):
-        handler = SlackHandler(handler_id, msg, reply, run, con, channel)
+    def add_handler(self, handler_id, msg, reply, run=None, channel=None):
+        msgs = []
+        replys = []
+        if type(reply) is str:
+            replys.append(reply.lower())
+        else:
+            replys = reply
+
+        if type(msg) is str:
+            msgs.append(msg.lower())
+        else:
+            for i in range(len(msg)):
+                msg[i] = msg[i].lower()
+            msgs = msg
+
+        handler = SlackHandler(handler_id, msgs, replys, run, channel)
         self.handlers.append(handler)
 
-    def add_handler_csv(self, data, delimit=","):
-        items = []
+    def add_handler_json(self, data):
         with open(data) as f:
-            reader = csv.reader(f, delimiter=delimit)
-            for row in reader:
+            reader = json.load(f)
+            for item in reader["handlers"]:
                 try:
-                    if len(row) == 3:
-                        self.add_handler(row[0], row[1], row[2].encode("utf-8").decode('unicode_escape'))
-                    elif len(row) == 4:
-                        functions = globals().copy()
-                        #try:
-                        #    self.add_handler(row[0], row[1], functions.get(row[2]))
-                        #except Exception as e:
-                        #    print("Function " + str(row[2]) + " not found\n" + str(e))
-                        #    break
+                    for i in item["reply"]:
+                        i = i.encode("utf-8").decode("unicode_escape")
+                    for i in item["message"]:
+                        i = i.lower()
+                    self.add_handler(item['id'], item['message'], item['reply'])
                 except Exception as e:
                     print("Error adding handler from CSV")
                     print(e)
@@ -54,7 +64,7 @@ class SlackBot(object):
 
     def watch_message(self):
         if self.connect:
-            self.sc.server.login_data["self"]["id"]
+            self.user = self.sc.server.login_data["self"]["id"]
             while True:
                 message = self.sc.rtm_read()
                 if message:
@@ -65,6 +75,8 @@ class SlackBot(object):
                             if self.debug:
                                 print("Message - " + text)
                             if self.user in text:
+                                text = text.replace("<@" + self.user + ">", "")
+                                print(text)
                                 self.parse_message(text, message[0]["channel"])
                     except:
                         #excepts when the message doesn't have a type, just ignore and keep going
@@ -73,14 +85,15 @@ class SlackBot(object):
 
     def parse_message(self, message, channel):
         for handler in self.handlers:
-            if handler.con:
-                if handler.message != "NULL":
-                    if handler.message.lower() in message.lower() or message.lower() in handler.message.lower():
-                        handler.received = message
-                        self.run_handler(handler, channel)
-
-            elif handler.message == message:
-                handler.run()
+            for msg in handler.message:
+                if msg in message.lower():
+                    handler.received = message
+                    self.run_handler(handler, channel)
+                    return 0
+            if message.lower() in handler.message:
+                handler.received = message
+                self.run_handler(handler, channel)
+                return 0
 
     def run_handler(self, handler, channel):
         if handler.run != None:
@@ -91,26 +104,28 @@ class SlackBot(object):
             except:
                 print("Exception during handler function")
 
-        if handler.reply != "" and handler.reply != None:
+        print(handler.reply)
+        if handler.reply != [""] and handler.reply != None:
             if handler.channel != None:
                 channel = handler.channel
             if channel == None:
                 print("Error: No channel selected")
                 return 1
+
+            reply = random.choice(handler.reply)
             if self.debug:
-                print("Reply: " + str(channel) + "  - " + str(handler.reply))
-            self.sc.rtm_send_message(str(channel), str(handler.reply))
+                print("Reply: " + str(channel) + "  - " + str(reply))
+            self.sc.rtm_send_message(str(channel), str(reply))
 
 
 
 
 class SlackHandler(object):
-    def __init__(self, handler_id, message, reply, run, con, channel):
+    def __init__(self, handler_id, message, reply, run, channel):
         self.handler_id = handler_id
         self.message = message
         self.reply = reply
         self.run = run
-        self.con = con
         self.channel = channel
         self.received = None
 
