f7baa303aadc7524f8dcc40c9601c963a214e144
Greg McCoy <gregmccoy@gfa.org>
2016-04-12 01:07:10 +0000

    Added Google Style Docstrings

--- pyslackbot/pyslackbot.py
+++ pyslackbot/pyslackbot.py
@@ -5,6 +5,15 @@ import json
 import random
 
 class SlackBot(object):
+    """Handles Slackbot interface
+
+    Base class from the slackbot, create a bot and listens for messages.
+    Handles messages using handlers.
+
+    Args:
+        bot_id (str): The SlackAPI token used to create the bot.
+        debug (bool): Determines if debug mode is enabled.
+    """
     def __init__(self, bot_id, debug=False):
         self.sc = SlackClient(bot_id)
         self.user = ""
@@ -19,16 +28,39 @@ class SlackBot(object):
         self.handlers = []
 
     def trigger_handler(self, handler_id):
+        """Triggers handler based on ID
+
+        Args:
+            handler_id (str): Unique ID of handler.
+        """
         handler = self.get_handler(handler_id)
         self.run_handler(handler, handler.channel)
 
     def get_handler(self, handler_id):
+        """Gets a handler based on ID
+
+        Args:
+            handler_id (str): Unique ID of handler.
+
+        Returns:
+            Handler if ID matches a handler, otherwise None
+        """
         for h in self.handlers:
             if h.handler_id == handler_id:
                 return h
         return None
 
     def add_handler(self, handler_id, msg, reply, run=None, channel=None):
+        """Creates a handler
+
+        Args:
+            handler_id (str): Unique ID of handler.
+            msg (str): Message that the handler will watch for.
+            reply (str): Reply to be send when message is recieved.
+            run (Optional [func]): function to be run when message is recieved.
+            channel (Optional [str]): Channel reply will be sent to.
+
+        """
         msgs = []
         replys = []
         if type(reply) is str:
@@ -47,6 +79,12 @@ class SlackBot(object):
         self.handlers.append(handler)
 
     def add_handler_json(self, data):
+        """Creates a handler based on json file
+
+        Args:
+            data (str): File location of json file.
+
+        """
         with open(data) as f:
             reader = json.load(f)
             for item in reader["handlers"]:
@@ -63,6 +101,13 @@ class SlackBot(object):
 
 
     def watch_message(self):
+        """Watchs for message on slack
+
+        Thread that constatnly watchs for new messages, If the bot is tagged
+        in the message it will check the handlers to see if there is a handler
+        for that message.
+
+        """
         if self.connect:
             self.user = self.sc.server.login_data["self"]["id"]
             while True:
@@ -84,6 +129,12 @@ class SlackBot(object):
                 time.sleep(1)
 
     def parse_message(self, message, channel):
+        """Checks message for handler
+
+        Args:
+            message (str): Message that was recieved
+            channel (str): Channel of the message recieved
+        """
         for handler in self.handlers:
             for msg in handler.message:
                 if msg in message.lower():
@@ -96,6 +147,15 @@ class SlackBot(object):
                 return 0
 
     def run_handler(self, handler, channel):
+        """Sends reply and run handler functions
+
+        Sends the reply message over slack and executes the function if
+        it is not None.
+
+        Args:
+            handler (:class:`SlackHandler`): Handler that needs to be triggered
+            channel (str): Channel of the message recieved
+        """
         if handler.run != None:
             if self.debug:
                 print("Excuting - " + str(handler.run))
@@ -122,6 +182,19 @@ class SlackBot(object):
 
 
 class SlackHandler(object):
+    """Handle message triggers and replies
+
+    Class used to create handlers for messages and replies.
+
+    Args:
+        handler_id (str): A unique ID for the handler.
+        message (list):  List of messages that with trigger the handler.
+        reply (list): List of replies to send back when the message is received.
+        org_reply (list): Copy of reply to allow reseting reply after it is formatted.
+        run (func): An function that will be run when message is received.
+        channel (channel): Channel that the reply will be sent to.
+        received (channel): Initalizes as None, will be set to the message recievied.
+    """
     def __init__(self, handler_id, message, reply, run, channel):
         self.handler_id = handler_id
         self.message = message
